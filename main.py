2d7da0a5dc982702bf05bf57b0563f53275b5975
Vaishnavi Joshi <vaishjoshi099@gmail.com>
2026-02-24 23:06:52 +0530

    'db_connection'

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import Depends,FastAPI
 from models import Product
+from database import session
+import database_models
+from database import engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+database_models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def greet():
@@ -14,21 +20,47 @@ products = [
     Product(id=4,name="shirt",description="Rare rabbit shirt", price=1000,quantity=15),
 ]
 
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    
+    db = session()
+    
+    count = db.query(database_models.Product).count
+    
+    if count == 0 :
+        for product in products:
+            db.add(database_models.Product(**product.model_dump()))
+    db.commit()
+    
+init_db()
+
 @app.get("/products")
-def get_all_products():
-    return products
+def get_all_products(db : Session= Depends(get_db)):
+    # db connection
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 @app.get("/product/{id}")
-def get_one_product(id : int):
-    for product in products:
-        if product.id == id:
-            return product
-        
+def get_one_product(id : int, db : Session = Depends(get_db)):
+    # for product in products:
+    #     if product.id == id:
+    #         return product 
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
     return "product not found!!!"
 
 @app.post("/product")
-def add_product(product : Product):
-    products.append(product)
+def add_product(product : Product, db : Session = Depends(get_db)):
+    # products.append(product)
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
     return product
 
 @app.put("/product/{id}")
